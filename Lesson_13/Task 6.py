041df4c541cb17ed94dfd57d8c46844d6cddf51c
Dmitriy Konstantinov <konstantinov.kiev@gmail.com>
2023-08-15 04:03:11 +0300

    Generator function was updated with 'yield'

--- Lesson_13/Task 6.py
+++ Lesson_13/Task 6.py
@@ -1,7 +1,9 @@
 # Напишіть генератор, який фільтрує непарні числа з заданої послідовності.
 
 def even_number_filter(number_list):
-    return (number for number in number_list if number % 2 == 0)
+    for number in number_list:
+        if number % 2 == 0:
+            yield number
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
